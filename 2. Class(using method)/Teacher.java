c866831a0133c63a17ddea2878b9f03a35e07855
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-09-17 22:38:51 +0600

    Add files via upload

---
+++ 2. Class(using method)/Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher {
+    String name, gender,phone;
+    int age;
+    void displayInformation()
+    {
+        System.out.println("Name   : "+name);
+        System.out.println("Gender : "+gender);
+        System.out.println("Age    : "+age);
+        System.out.println("Phone  : "+phone);
+        System.out.println("\n");
+    }
+}
---
+++ 2. Class(using method)/test.java
@@ -0,0 +1,18 @@
+public class test {
+    public static void main(String[]args)
+    {
+        Teacher teacher1= new Teacher();
+        teacher1.name= "Rhedoy";
+        teacher1.gender= "Male";
+        teacher1.phone="01303517139";
+        teacher1.age=22;
+        teacher1.displayInformation();
+
+        Teacher teacher2= new Teacher();
+        teacher2.name= "Onti";
+        teacher2.gender= "Female";
+        teacher2.phone="01761198246";
+        teacher2.age=22;
+        teacher2.displayInformation();
+    }
+}
