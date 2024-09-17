9279c5ff99cb714f400fb52a1e1dde4ef593e9de
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-09-17 23:21:35 +0600

    Add files via upload

---
+++ 4. Constructor/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[]args)
+    {
+        Teacher teacher1= new Teacher("Rhedoy","Male","01303517139",22);
+        teacher1.displayInformation();
+
+        Teacher teacher2= new Teacher("Onti","Female","01761198246",22);
+        teacher2.displayInformation();
+    }
+}
