d22bb367592de00bc94ee598118ca4a0f35d43e0
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-09-26 01:56:35 +0600

    Add files via upload

---
+++ 10. Static variable 3/student.java
@@ -0,0 +1,12 @@
+public class student {
+    static int count =0;
+    student()
+    {
+        count++;
+    }
+    void display()
+    {
+        System.out.println("students are= "+count);
+    }
+}
+
