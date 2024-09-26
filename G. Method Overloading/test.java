a604048aa8fa5c78e9fd7b45527c6efabaa7a96e
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-09-27 00:59:13 +0600

    Add files via upload

---
+++ G. Method Overloading/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[]  args)
+    {
+        MethodOverload ob = new MethodOverload();
+        ob.add(5,6);
+        ob.add(5,6,7);
+        ob.add(5.6,7.8);
+
+    }
+}
