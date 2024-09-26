f89402debaf1ddfa85031cf21a533c289d194296
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-09-26 11:34:18 +0600

    Add files via upload

---
+++ C. Static Block/student.java
@@ -0,0 +1,15 @@
+public class student {
+    static int id;         //staic block;
+    static String name;
+    static
+    {
+        id=1385;
+        name= "Rhedoy";
+    }
+   static void display()
+    {
+        System.out.println("Id= "+id);
+        System.out.println("Name= "+name);
+    }
+}
+
