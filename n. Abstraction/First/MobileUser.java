0375de94156e3b37cf70290d1e87c06326838a1d
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-10-12 22:19:14 +0600

    Add files via upload

---
+++ n. Abstraction/First/MobileUser.java
@@ -0,0 +1,7 @@
+public abstract class MobileUser {
+    void Display()
+    {
+        System.out.println("Blue wave");
+    }
+    abstract void sentMessage();
+}
---
+++ n. Abstraction/First/Rahim.java
@@ -0,0 +1,6 @@
+public class Rahim extends MobileUser{
+    @Override
+    void sentMessage() {
+        System.out.println("I am Rahim");
+    }
+}
