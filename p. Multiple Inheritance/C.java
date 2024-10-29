006e94ca6a7b22fc6a91ec0c7dca3b85a3955082
Md. Nur Hossain Rhedoy <143288716+Mr-Rhedoy@users.noreply.github.com>
2024-10-30 01:07:06 +0600

    Add files via upload

---
+++ p. Multiple Inheritance/C.java
@@ -0,0 +1,12 @@
+public class C implements A, B{
+    @Override
+    public void display() {
+        System.out.println("I love you");
+    }
+
+    @Override
+    public void display2()
+    {
+        System.out.println("I love you 2");
+    }
+}
